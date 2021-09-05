72676d32fb89daf31db09dd45e067c6ba0c9b7c7
205g0 <205g0@protonmail.com>
2021-09-05 14:04:06 +0200

    answer

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import { selectAll } from 'unist-util-select'
-import type { Heading } from 'mdast'
+import type { Heading, Parent } from 'mdast'
+import type { Node } from 'unist'
+import { is } from 'unist-util-is'
 
 const someText: string = `
 # This is some markdown
@@ -11,6 +13,8 @@ const someText: string = `
 And a paragraph
 `
 
+interface Data extends Parent {}
+
 class Something {
   constructor(someText: string) {
     const ast = unified().use(remarkParse).parse(someText)
@@ -18,9 +22,13 @@ class Something {
       .use(remarkParse)
       .parse(someText)
 
-    selectAll('root > *', ast).forEach((node: Heading) => {
-      if (node.depth == 2) {
-        console.log('I found a heading with level 2')
+    interface Data {}
+
+    selectAll('root > *', ast).forEach((node: Node) => {
+      if (is<Heading>(node, 'heading')) {
+        if (node.depth == 2) {
+          console.log('I found a heading with level 2')
+        }
       }
     })
   }
